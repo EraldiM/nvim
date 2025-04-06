91e134bf31dac325aff2644af7d542414f2c3778
Eraldi <eraldimici00@gmail.com>
2025-04-06 11:43:29 +0200

    vediamo se funziona

---
+++ lua/options.lua
@@ -0,0 +1,18 @@
+vim.opt.number = true
+
+vim.opt.relativenumber = true
+vim.opt.wrap = false
+
+vim.opt.expandtab  = true
+vim.opt.tabstop = 4 -- tab viene sostituito con 4 spazi
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus" -- il buffer per copiare e incollare è sincronizzato con il sistema
+vim.opt.scrolloff = 999 -- fa si che la linea corrente sia sempre al centro dello schermo
+
+vim.opt.virtualedit = "block"
+
+vim.opt.inccommand = "split"
+
+vim.opt.termguicolors = true
+
+
